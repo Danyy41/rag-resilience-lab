ac052fc731c8d6f2e71c0f53ec313775d4b62872
Danyy41 <dany.mhaidly@gmail.com>
2026-04-18 19:53:23 -0400

    Update evaluate.py

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -2,7 +2,8 @@ import json
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "app"))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 
 from ingest import build_index
 import numpy as np
