56213267ca5a81e259ea0fad66162da712bd4cf9
Danyy41 <dany.mhaidly@gmail.com>
2026-04-18 19:54:53 -0400

    Update run_defended.py

--- eval/run_defended.py
+++ eval/run_defended.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.dirname(__file__))
+
 from evaluate import evaluate
 
 if __name__ == "__main__":
